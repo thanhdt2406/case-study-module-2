1e2c02c0f0d771145dbac5f0eb2ca33b5ac74cb5
viet <vietdq2412@gmail.com>
2020-10-09 14:11:43 +0700

    update inputer

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,14 @@ import command.AddProduct;
 import command.Command;
 import command.ProductComander;
 import command.ShowAllProduct;
+import entity.Product;
+import io.Inputer;
 import manager.product.ProductManager;
 
 public class Main {
 
     public static void main(String[] args) {
+        Inputer inputer = new Inputer();
         ProductManager manager = new ProductManager();
 
         Command addProduct = new AddProduct(manager);
@@ -16,9 +19,14 @@ public class Main {
 
         ProductComander productComander = new ProductComander(addProduct,showAllProduct);
 
-        productComander.chooseAddProduct();
+        System.out.println("enter product name: ");
+        String name = inputer.inputString();
+        Product newP = new Product(name);
+        productComander.chooseAddProduct(newP);
         System.out.println("add2: ");
-        productComander.chooseAddProduct();
+        name = inputer.inputString();
+        Product newP2 = new Product(name);
+        productComander.chooseAddProduct(newP2);
 
         System.out.println("showww: ");
         productComander.chooseShowAllProduct();
