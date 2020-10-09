b2593e64c66633074d1ae38b84809efe5e4a6e01
thanhdt246 <dothanh24062000@gmail.com>
2020-10-09 14:38:37 +0700

    add storage

--- src/storage/IOFileManager.java
+++ src/storage/IOFileManager.java
@@ -2,13 +2,13 @@ package storage;
 
 import java.util.HashMap;
 
-public class IOFileManager<T,E> {
+public class IOFileManager<T, E> {
 
-    public HashMap<T,E> getData(String fileName){
-
+    public HashMap<T, E> getData(String fileName) {
+        return null;
     }
 
-    public void updateData(HashMap map,String fileName){
+    public void updateData(HashMap map, String fileName) {
 
     }
 }
