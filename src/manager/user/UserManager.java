c323e757859e242b196ebb0bc19977f14b55cf65
thanhdt246 <dothanh24062000@gmail.com>
2020-10-09 14:26:18 +0700

    clear test

--- src/manager/user/UserManager.java
+++ src/manager/user/UserManager.java
@@ -1,36 +1,16 @@
 package manager.user;
 
-<<<<<<< HEAD
-
-import java.util.HashMap;
 
 public class UserManager implements IUserManager {
-    HashMap listUser = new HashMap();
-=======
-import entity.User;
->>>>>>> 11245bfca3309f454cd00bd233d1203ccfe83d47
 
-import java.util.HashMap;
-
-public class UserManager implements IUserManager<User> {
-
-    HashMap<String, User> userMap = new HashMap<>();
     @Override
-<<<<<<< HEAD
-    public boolean createUser() {
-
-=======
-    public boolean addUser(User user) {
-        userMap.put(user.getUserName(), user);
->>>>>>> 11245bfca3309f454cd00bd233d1203ccfe83d47
+    public boolean addUser(Object o) {
         return false;
     }
 
     @Override
-    public void showAllUser(){
-        for (User ele : userMap.values()){
-            System.out.println(ele.toString());
-        }
+    public void showAllUser() {
+
     }
 
     @Override
---
+++ src/storage/IOFileManager.java
@@ -0,0 +1,14 @@
+package storage;
+
+import java.util.HashMap;
+
+public class IOFileManager<T,E> {
+
+    public HashMap<T,E> getData(String fileName){
+
+    }
+
+    public void updateData(HashMap map,String fileName){
+
+    }
+}
